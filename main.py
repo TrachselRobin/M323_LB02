f2699eda78f420453a0111b43a2d56ad46f559ad
trachselr <trachselr@bzz.ch>
2024-11-08 15:02:31 +0100

    INITIALIZED: LB02

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+    AUTHOR:  Robin Trachsel
+    VERSION: 1.0
+    DATE:    08.11.2024
+
+    DESCRIPTION: Main für die Flask Applikation der LB02
+"""
+
+# imported Modules
+# from flask import Flask
+
+
+# imported Files
+# from test import Test
+
+
+def helloWorld(text):
+    print(text)
+
+
+if __name__ == "__main__":
+    helloWorld("print")
